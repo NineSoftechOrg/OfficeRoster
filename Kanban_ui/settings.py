4c9e245db812d8dc4b934bf13f90ff314965436f
NineSoftechOrg <122348658+NineSoftechOrg@users.noreply.github.com>
2024-07-11 18:27:51 +0530

    Update settings.py

--- Kanban_ui/settings.py
+++ Kanban_ui/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a@7_(i+v^-%dtj!kc*-)e+%b*688rwm9)yop$!0!%_l+_d=#(v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["15.207.99.220"]
 
