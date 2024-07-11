7f7dc401a611d04fcfda33b9ccac2eb4131bbf2d
NineSoftechOrg <122348658+NineSoftechOrg@users.noreply.github.com>
2024-07-11 16:02:54 +0530

    Update settings.py

--- Kanban_ui/settings.py
+++ Kanban_ui/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a@7_(i+v^-%dtj!kc*-)e+%b*688rwm9)yop$!0!%_l+_d=#(v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".awsapprunner.com"]
 
 
 # Application definition
