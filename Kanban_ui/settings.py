51e3f72d965870fcf5c32054616f8355a179ddf1
NineSoftechOrg <122348658+NineSoftechOrg@users.noreply.github.com>
2024-07-12 11:14:11 +0530

    Update settings.py

--- Kanban_ui/settings.py
+++ Kanban_ui/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
     "default": {
